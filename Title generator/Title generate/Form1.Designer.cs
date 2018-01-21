e33d5a2359424e9cc6098b17b6e96b9e6a38b385
bubu-mlesio <xd96-96@o2.pl>
2018-01-21 12:20:56 +0100

    first app on github, final version

---
+++ Title generator/Title generate/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Title_generate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title_name = new System.Windows.Forms.TextBox();
+            this.models_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fromFile = new System.Windows.Forms.CheckBox();
+            this.file_name = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // title_name
+            // 
+            this.title_name.Location = new System.Drawing.Point(17, 107);
+            this.title_name.Multiline = true;
+            this.title_name.Name = "title_name";
+            this.title_name.Size = new System.Drawing.Size(521, 29);
+            this.title_name.TabIndex = 0;
+            this.title_name.Text = "Wspólny tytuł";
+            // 
+            // models_name
+            // 
+            this.models_name.Location = new System.Drawing.Point(17, 188);
+            this.models_name.MaxLength = 0;
+            this.models_name.Multiline = true;
+            this.models_name.Name = "models_name";
+            this.models_name.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.models_name.Size = new System.Drawing.Size(521, 296);
+            this.models_name.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tytuł:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Modele:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 506);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(521, 100);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fromFile
+            // 
+            this.fromFile.AutoSize = true;
+            this.fromFile.Location = new System.Drawing.Point(22, 12);
+            this.fromFile.Name = "fromFile";
+            this.fromFile.Size = new System.Drawing.Size(173, 29);
+            this.fromFile.TabIndex = 6;
+            this.fromFile.Text = "modele z pliku?";
+            this.fromFile.UseVisualStyleBackColor = true;
+            this.fromFile.CheckedChanged += new System.EventHandler(this.fromFile_CheckedChanged);
+            // 
+            // file_name
+            // 
+            this.file_name.Location = new System.Drawing.Point(22, 47);
+            this.file_name.Multiline = true;
+            this.file_name.Name = "file_name";
+            this.file_name.Size = new System.Drawing.Size(171, 29);
+            this.file_name.TabIndex = 7;
+            this.file_name.Text = "modele";
+            this.file_name.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 655);
+            this.Controls.Add(this.file_name);
+            this.Controls.Add(this.fromFile);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.models_name);
+            this.Controls.Add(this.title_name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox title_name;
+        private System.Windows.Forms.TextBox models_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox fromFile;
+        private System.Windows.Forms.TextBox file_name;
+    }
+}
+
---
+++ Title generator/Title generate/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Title_generate
+{
+    public partial class Form1 : Form
+    {
+        string[] allLines;
+        List<string> title = new List<string>();
+        List<string> bug_words = new List<string>();
+        List<string> models = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            read_models(models_name.Text);
+            //check main title
+            check_string(title_name.Text);
+            
+            string tmp = title_name.Text;
+            //create titles with models
+            for (int i =0; i<models.Count(); i++)
+            {
+
+                if (tmp.Length < 50)
+                {
+                    tmp += " " + models[i];
+                }
+                else if (tmp.Length == 50)
+                {
+                    title.Add(tmp);
+                    tmp = title_name.Text + " " + models[i];
+                }
+                else if (tmp.Length > 50)
+                {
+                    int num = tmp.Length - (models[i - 1].Length + 1);
+                    title.Add(tmp.Substring(0, num));
+                    tmp = title_name.Text;
+                    i = i - 2;
+                }
+                if (i == models.Count()-1)
+                {
+                    if (models[i] != "\n")
+                        {
+                            title.Add(tmp);
+                            tmp = null;
+                        }
+                }
+                Thread.Sleep(10);
+            }
+            //save titles
+            save("title.txt", false);
+            //clean collections
+            title.Clear();
+            bug_words.Clear();
+            models.Clear();
+        }
+        //check main title
+        private void check_string (string name)
+        {
+            if(name.Length >= 50)
+            {
+                MessageBox.Show("Tytuł jest za długi", "ERROR 404", MessageBoxButtons.OK);
+                this.Close();
+            }
+            string ttt;
+            for (int i = 0; i < allLines.Count(); i++)
+            {
+                ttt = name + " " + allLines[i];
+                if (ttt.Length > 50)
+                {
+                    bug_words.Add(allLines[i] + "\n");
+                    allLines[i] = "";
+                }
+                    
+            }
+            foreach(string model in allLines)
+            {
+                if(model != "")
+                {
+                    models.Add(model);
+                }
+            }
+            if (bug_words.Count() > 0)
+            {
+                //save to file incorrect models
+                save("models_too_long.txt", true);
+                MessageBox.Show("Lista za długich modeli zapisuję do pliku models_too_long.txt", "404", MessageBoxButtons.OK);
+            }
+
+
+        }
+        private void read_models(string models)
+        {
+            //load from text box
+            if (fromFile.CheckState.ToString() == "Unchecked")
+            {
+                if (models.Length > 0)
+                {
+                    allLines = models.Split('\n');
+                    for (int i = 0; i < allLines.Count(); i++)
+                    {
+                        if (i != allLines.Count() - 1)
+                            allLines[i] = allLines[i].Substring(0, allLines[i].Length - 1);
+                    }
+                }
+                else
+                    MessageBox.Show("Ilość modeli jest zbyt mała", "404 ERROR", MessageBoxButtons.OK);
+
+            }
+        //load from file
+            else
+            {
+                if (File.Exists(file_name.Text + ".txt"))
+                    allLines = File.ReadAllLines(file_name.Text + ".txt", Encoding.UTF8);
+                else
+                    MessageBox.Show("Brak wskazanego pliku " +file_name.Text + ".txt", "404 ERROR", MessageBoxButtons.OK);
+            }
+         }
+        //save to file
+        private void save(string file_name, bool models_name)
+        {
+
+            try
+            {
+                StreamWriter sw = new StreamWriter(file_name, true);
+                if (models_name == false)
+                {
+                    foreach (string tekst in title)
+                        sw.WriteLine(tekst);
+                }
+                else
+                {
+                    foreach(string tekst in bug_words)
+                        sw.WriteLine(tekst);
+                }
+                sw.WriteLine(" ");
+                sw.Close();
+                MessageBox.Show("Zapis pomyślny!", "Success!", MessageBoxButtons.OK);
+            }catch (Exception e)
+            {
+                MessageBox.Show("Wystąpił błąd: " + e.ToString(), "404", MessageBoxButtons.OK);
+            }
+        }
+
+        private void fromFile_CheckedChanged(object sender, EventArgs e)
+        {
+            if (file_name.Visible == true)
+            {
+                file_name.Visible = false;
+            }
+            else
+                file_name.Visible = true;
+        }
+    }
+}
